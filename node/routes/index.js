816296cada5e8e8a0e1b63604cdd3a2527c1e33f
theballmarcus <mcihme@gmail.com>
2021-02-03 14:51:25 +0100

    Added index that displays all the files in /public/manual/

--- node/routes/index.js
+++ node/routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs');
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  manuals = []
+  fs.readdir("./public/manual", (err, files) => {
+    if (err) console.log(err)
+    files.forEach(file => {
+      manuals.push(file)
+      console.log(file)
+    });
+
+    res.render('index', { title: 'Express', manuals: manuals});
+  });
 });
 
 module.exports = router;
