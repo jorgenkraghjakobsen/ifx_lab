f6a19dede7ca09f733c496b41de0dc99c2aaf27d
theballmarcus <mcihme@gmail.com>
2021-02-04 16:30:57 +0100

    Added first database and database call.

--- node/routes/index.js
+++ node/routes/index.js
@@ -1,21 +1,23 @@
 var express = require('express');
 var router = express.Router();
 const fs = require('fs');
-
+var sqlib = require("../sqlite3_lib.js")
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  manuals = []
-  fs.readdir("./public/manual", (err, files) => {
-    if (err) console.log(err)
-    files.forEach(file => {
-      manuals.push(file)
-      console.log(file)
+    manuals = []
+    fs.readdir("./public/manual", (err, files) => {
+        if (err) console.log(err)
+        files.forEach(file => { 
+            manuals.push(file)
+      //console.log(file)
+        });
+        sqlib.readTable("./data/instruments.db", "instruments", (rows) => {
+            console.log(rows)
+            res.render('index', { title: 'Express', manuals: manuals, instruments : rows});
+        })
     });
-
-    res.render('index', { title: 'Express', manuals: manuals});
-  });
 });
 
 module.exports = router;
