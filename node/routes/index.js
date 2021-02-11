9698f95fca33aa2237b67523d49e8ff2892cf452
theballmarcus <mcihme@gmail.com>
2021-02-11 14:58:29 +0100

    Added more manuals and another field that shows weather or not there is an avaiable manual for the specific instrument.

--- node/routes/index.js
+++ node/routes/index.js
@@ -9,12 +9,17 @@ router.get('/', function(req, res, next) {
     manuals = []
     fs.readdir("./public/manual", (err, files) => {
         if (err) console.log(err)
-        files.forEach(file => { 
-            manuals.push(file)
-      //console.log(file)
-        });
+
         sqlib.readTable("./data/instruments.db", "instruments", (rows) => {
             //console.log(rows)
+            files.forEach(file => { 
+                manuals.push(file)
+          //console.log(file)
+            });
+            for(i in rows) {
+                j = manuals.includes(rows[i].model + ".pdf")
+                rows[i].manual = j
+            }
             res.render('index', { title: 'Express', manuals: manuals, instruments : rows});
         })
     });
